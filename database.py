26f79e390b76fbafcc2af43950c436469cf55434
SonsoriIssah <issonsori@gmail.com>
2026-08-03 16:03:57 +0000

    Update your  to read from environment variables

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy.ext.asyncio import create_async_engine,AsyncSession
 from sqlalchemy.orm import DeclarativeBase,sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost/fastapi_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
 engine = create_async_engine(DATABASE_URL,echo=True) #This is what creates the a connection to the database 
 AsyncSessionLocal = sessionmaker(engine,class_=AsyncSession,expire_on_commit=False)
 
